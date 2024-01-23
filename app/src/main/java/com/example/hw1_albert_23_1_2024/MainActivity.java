35b3da78bf1a8c7352a62e5afe08d5c976704b0e
Lior1254 <liorshemtov2008@gmail.com>
2024-01-23 10:17:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hw1_albert_23_1_2024/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hw1_albert_23_1_2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
